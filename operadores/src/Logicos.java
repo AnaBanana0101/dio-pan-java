58cd5a11ecb973d67827fe2c007c1586c43bcedc
Ana Luiza Reis <120499627+AnaBanana0101@users.noreply.github.com>
2023-05-11 13:53:33 -0300

    Estudo sobre operadores

---
+++ operadores/src/Logicos.java
@@ -0,0 +1,24 @@
+public class Logicos {
+
+        //contemplam valores booleanos ou numerais
+
+        public static void main(String[] args) { 
+
+            boolean condicao1 = true;
+            boolean condicao2 = true; 
+    
+            //condição "e" (and)
+            if(condicao1 && condicao2){
+                System.out.println("As duas condições são verdadeiras.");
+                }
+            
+            //condição "ou" (or)
+            if(condicao1 || condicao2){
+                System.out.println("Uma das condições é verdadeira. ");
+                }
+    
+            System.out.println("Fim");
+    
+        }
+    
+}
---
+++ operadores/src/Operacao.java
@@ -0,0 +1,16 @@
+public class Operacao {
+    
+    public static void main(String[] arg) {
+        int numero = 5; 
+
+        numero = - numero;
+        System.out.println(numero);
+        
+        //+ - * / são operadores aritméticos, para realizar conversao 
+        // necessita de uma multiplicação. 
+        numero = numero * - 1;
+        System.out.println(numero);
+
+
+    }
+}
